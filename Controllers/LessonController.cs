95e8fa654d226254af6d080ea3648ea6d2e6ce29
lehoangnam25112004 <lehoangnam25112004@gmail.com>
2026-09-10 22:25:16 +0700

    Lesson5

--- Controllers/LessonController.cs
+++ Controllers/LessonController.cs
@@ -8,5 +8,10 @@ namespace DemoMVC.Controllers
         {
             return View();
         }
+
+         public IActionResult Lesson5()
+        {
+            return View();
+        }
     }
 }
